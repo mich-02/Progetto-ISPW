44195836710ddf1477b31a448740519f6aa9c38f
mich-02 <michelebiru@gmail.com>
2025-09-09 12:02:27 +0200

    modificato DaoProvider

--- Sorgenti_Foodie/src/com/foodie/model/dao/DaoFactoryProvider.java
+++ Sorgenti_Foodie/src/com/foodie/model/dao/DaoFactoryProvider.java
@@ -6,7 +6,7 @@ import com.foodie.model.dao.db.DBDaoFactory;
 import com.foodie.model.dao.memo.MemoDaoFactory;
 
 public final class DaoFactoryProvider {
-	private static DaoFactoryProvider istanza; 
+	private static DaoFactoryProvider istanza = null; 
     private DaoFactory daoFactory; 
 
     // Costruttore privato per impedire istanziazione esterna
@@ -32,26 +32,4 @@ public final class DaoFactoryProvider {
     public DaoFactory getDaoFactory() {
         return daoFactory;
     }
-	/*
-	private static DaoFactory istanza;
-
-    // Costruttore privato per impedire istanziazione
-    private DaoFactoryProvider() {
-    	
-    }
-
-    public static DaoFactory ottieniIstanza() {
-        if (istanza == null) {
-            Persistenza persistenza = ConfiguratorePersistenza.getPersistenzaCorrente();
-            if (persistenza == Persistenza.MEMORIA) {
-                istanza = new MemoDaoFactory();
-            } else if (persistenza == Persistenza.DATABASE) {
-                istanza = new DBDaoFactory();
-            } else {
-                throw new IllegalStateException("Persistenza non configurata");
-            }
-        }
-        return istanza;
-    }
-    */
 }
